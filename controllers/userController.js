ee6443e70e6b100c8ec08808bdc5f0cae1b2a9f5
Alamin <pg2@banglafire.com>
2018-08-06 15:35:34 +0600

    changpassword done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,6 @@
 let bcrypt = require('bcrypt');
 let response = require('../helper/response');
 let UserModel = require('../models/userModel');
-let MessModel = require('../models/messModel');
 const saltRounds = 10;
 
 class UserController {
@@ -39,8 +38,41 @@ class UserController {
         });
 
     };
+    changePassword(req, res){
+        let id = req.auth.id;
+        let oldPassword = req.body.oldPassword;
+        let newPassword = req.body.newPassword;
+
+        UserModel.findOne({_id: id},(err,user)=>{
+            if(err){
+                return res.json(response.error(false,"An error occur",err))
+            } else {
+
+                  bcrypt.compare(oldPassword,user.password,(err,isMatched)=>{
+                      if(isMatched){
+                          bcrypt.hash(newPassword,saltRounds,(err,hashed)=> {
+                              if (err) {
+                                  return res.json(response.error(false,"An error occur",err))
+                              } else {
+
+                                  UserModel.update({_id: id},{ $set: { password: hashed}}, { new: true }, (err,data)=>{
+                                      if(err){
+                                          return res.json(response.error(false,'An error occur',err))
+                                      } else {
+                                          return res.json(response.single(true, 'Password changed successfully', data))
+                                      }
+                                  });
+                              }
+                          });
 
 
+                      } else {
+                          return res.json(response.error(false, 'Password does not matched',err))
+                      }
+                  });
+            }
+        });
+    }
 
 }
 
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,9 @@ const UserController = require('../controllers/userController');
 const BalanceController = require('../controllers/balanceController');
 const ExpenseController = require('../controllers/expenseController');
 
+
+    router.route('/changePassword').put(AuthController.isUser, UserController.changePassword)
+
     router.route('/addUser').post(AuthController.isAdmin, UserController.addUser);
     router.route('/addBalanceCategory').post(AuthController.isAdmin, BalanceController.addBalanceCategory );
     router.route('/addBalance').post(AuthController.isUser, BalanceController.addBalance );
