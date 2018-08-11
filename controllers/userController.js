114b8e3577386f501cc4538b8deb318e76d1253e
Miraje <mdalamin8521@gmail.com>
2018-08-11 15:42:03 +0600

    user controller code clean and user lib modified

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,5 @@
-let bcrypt = require('bcrypt');
 let response = require('../helper/response');
-let UserModel = require('../models/userModel');
-
 const UserLib = require('../lib/user.lib');
-const saltRounds = 10;
 
 class UserController extends UserLib{
     constructor(){
@@ -12,92 +8,39 @@ class UserController extends UserLib{
 
     addUser(req,res){
         let user = req.body;
-        bcrypt.hash(user.password,saltRounds,(err,hashed)=>{
-            if(err){
-                return res.json(response.error(false,"An error occur hash password",err))
-            } else {
-                user.password = hashed;
-                user.role = 'user'; ///admin , user
-                user.messusername = req.auth.messusername;
-
-                let emailobj = {
-                    email: user.email
-                };
-                UserModel.findOne(emailobj,(err,found)=>{
-                    if(found !== null){
-                        return res.json(response.error(false,"Email already exist","Email already exist"));
-                    } else {
-                        UserModel.create(user, (err,result)=>{
-                            if(err){
-                                return res.json(response.error(false,"An error occur",err.message))
-                            } else {
-                                return res.json(response.single(true, "New User Created", result));
-                            }
-                        })
-                    }
-
-                });
-
-            }
+        super.addUser(user, req.auth.messusername).then(data=>{
+            return res.json(response.single(true, `New User Created`, data));
+        }).catch(err=>{
+            return res.json(response.error(false,"An error occur",err));
         });
-
     };
     changePassword(req, res){
         let id = req.auth.id;
         let oldPassword = req.body.oldPassword;
         let newPassword = req.body.newPassword;
-
-        UserModel.findOne({_id: id},(err,user)=>{
-            if(err){
-                return res.json(response.error(false,"An error occur",err))
-            } else {
-
-                  bcrypt.compare(oldPassword,user.password,(err,isMatched)=>{
-                      if(isMatched){
-                          bcrypt.hash(newPassword,saltRounds,(err,hashed)=> {
-                              if (err) {
-                                  return res.json(response.error(false,"An error occur",err))
-                              } else {
-
-                                  UserModel.update({_id: id},{ $set: { password: hashed}}, { new: true }, (err,data)=>{
-                                      if(err){
-                                          return res.json(response.error(false,'An error occur',err))
-                                      } else {
-                                          return res.json(response.single(true, 'Password changed successfully', data))
-                                      }
-                                  });
-                              }
-                          });
-
-
-                      } else {
-                          return res.json(response.error(false, 'Password does not matched',err))
-                      }
-                  });
-            }
+        super.changPassword(id,oldPassword,newPassword).then(data=>{
+            return res.json(response.single(true, `Password changed successfully`, data));
+        }).catch(err=>{
+            return res.json(response.error(false,"An error occur",err));
         });
     };
 
     updateProfile(req,res){
         const id = req.auth.id;
         let updateObject = req.body;
-        UserModel.findByIdAndUpdate(id, updateObject,{new:true}, (err,data)=>{
-            if(err){
-                return res.json(response.error(false,'An error occur',err));
-            } else {
-                return res.json(response.single(true, 'Updated successfully', data));
-            }
-        })
+        super.updateProfile(updateObject).then(data=>{
+            return res.json(response.single(true, `Profile Update successfully`, data));
+        }).catch(err=>{
+            return res.json(response.error(false,"An error occur",err));
+        });
     };
     getProfile(req,res){
         const id = req.auth.id;
-        UserModel.findById(id, {password:0}, (err,data)=>{
-            if(err){
-                return res.json(response.error(false,'An error occur',err));
-            } else {
-                return res.json(response.single(true, `Welcome ${data.username}`, data));
-            }
-        })
+        super.getProfile(id).then(data=>{
+            return res.json(response.single(true, `Welcome ${data.username}`, data));
+        }).catch(err=>{
+            return res.json(response.error(false,"An error occur",err));
+        });
     };
 
     getUsers(req,res){
