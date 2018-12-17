0b496ece1ad20bea6b7f45b6a4c6c00c957872ae
mirajehossain <mdalamin8521@gmail.com>
2018-12-18 00:41:20 +0600

    total meal, done

--- routes/user.js
+++ routes/user.js
@@ -64,11 +64,11 @@ const AuthValidation = require('../validation/authValidation');
  */
     router.route('/meal/addMeal').post(AuthController.isAdmin, MealController.addMeal);
     router.route('/meal/updateMeal/:mealId').put(AuthController.isAdmin, MealController.updateMeal);
+    router.route('/meal/deleteMeal/:mealId').delete(AuthController.isAdmin, MealController.deleteMeal);
     router.route('/meal/totalMealInMonth').get(AuthController.isUser, MealController.totalMealInMonth);
-    // router.route('/meal/totalMeal').get(AuthController.isUser, MealController.totalMeal);
     router.route('/meal/currentMeal').get(AuthController.isUser, MealController.currentMeal);
-    // router.route('/meal/userWiseMeal/:userId').get(AuthController.isUser, MealController.userWiseMeal);
     router.route('/meal/mealRateInMonth').get(AuthController.isUser, MealController.mealRateInMonth);
+    // router.route('/meal/userWiseMeal/:userId').get(AuthController.isUser, MealController.userWiseMeal);
     // router.route('/meal/mealRate').get(AuthController.isUser, MealController.mealRate);
     // router.route('/meal/currentMessMealWithRate').get(AuthController.isUser, MealController.currentMessMealWithRate);
 
