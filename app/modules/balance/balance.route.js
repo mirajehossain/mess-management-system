a8b85781651140f6490bd1d2fb50fed0426eb344
mirajehossain <mdalamin8521@gmail.com>
2019-02-11 23:23:19 +0600

    fixed route typo in balance

--- app/modules/balance/balance.route.js
+++ app/modules/balance/balance.route.js
@@ -15,7 +15,7 @@ const BalanceController = require('./balance.controller');
 router.route('/addBalance').post(AuthController.isAdmin, BalanceController.addBalance);
 router.route('/updateBalance/:balanceId').put(AuthController.isAdmin,  BalanceController.updateBalance);
 router.route('/deleteBalance/:balanceId').delete(AuthController.isAdmin,  BalanceController.deleteBalance);
-router.route('/totalMessBalance').get(AuthController.isUser, BalanceController.totalMessBalance);
+router.route('/messTotalBalance').get(AuthController.isUser, BalanceController.totalMessBalance);
 router.route('/userTotalBalance').get(AuthController.isUser, BalanceController.userTotalBalance );
 router.route('/userMealBalance').get(AuthController.isUser, BalanceController.userMealBalance );
 router.route('/categoryWiseBalance/:categoryId').get(AuthController.isUser,  BalanceController.categoryWiseBalance);
