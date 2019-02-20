49a0a2cb0034b512ada846fc2a2fe25a94d52195
mirajehossain <mdalamin8521@gmail.com>
2019-02-20 23:06:29 +0600

    api response collection removed

--- helper/response.js
+++ helper/response.js
@@ -4,11 +4,6 @@ module.exports = {
             success, message, data
         }
     },
-    collection: function (success = false, message = "", data = [], page = 1, count = 1, limit = 1) {
-        return {
-            success, message, page, limit, count, data
-        };
-    },
     error: function (success = false, message = "",error = "") {
         return {
             success , message , error
